f7a2d6a95c6d684e67cd6096e2e17eff7ee837ed
douyangyd <63422333+douyangyd@users.noreply.github.com>
2020-04-14 12:54:10 -0700

    generate all unique allocation

--- genallocs.R
+++ genallocs.R
@@ -1,7 +1,7 @@
 library(arrangements)
 
 ### The code was created by Liang Xu
-temp <- c(6,4,2) # # number of cluster for each type (S,L) or (S,M,L)
+temp <- c(6,4,2) # # number of cluster for each type (S,M,L) (only for three size group)
 M <- temp
 N = rep(3,4) # #of clus transit per period
 p = length(N) 
@@ -74,6 +74,112 @@ for(i in 1:dim(X6)[1]){
 v <- c()
 for(i in 1:dim(X6)[1]){
   v[i]<-multi(M,N,allocs,i)}
+w<-v/sum(v)## weights
+
+
+
+
+##===================================================
+## for two/three size groups. Would recommend above function for more efficient generation
+
+M = c(6,18) # #of clus transit per period
+N = rep(6,4) 
+p = length(N)
+
+
+## -------------------------------------------------------------------------
+## Part one: List all the unique allocations
+## -------------------------------------------------------------------------
+
+
+alloc1 = function(M.now, period=1, stem=vector(), store.alloc=F) {
+  #print("new")
+  #print(paste("M.now = ", M.now))
+  if (period ==  J) {
+    complete.alloc = cbind(stem, M.now)
+    #print(M)
+    if (all(apply(complete.alloc, 1, sum) == M)) {
+      count <<- count + 1
+      print(paste("count = ", count))
+      if (store.alloc) {
+        allocs <<- c(allocs, complete.alloc)
+      }
+    }
+  } else {
+    min.type = pmax(0, N[period] - sum(M.now) + M.now)
+    max.type = pmin(M.now, N[period])
+    #print(paste("min.type = ", min.type))
+    #print(paste("max.type = ", max.type))
+    n.per.type = max.type - min.type + 1
+    n.alloc = vector()
+    #print(paste("n.per.type", paste(n.per.type)))
+    #print(prod(n.per.type))
+    for (j in 0:(prod(n.per.type) - 1)) {
+      #print(paste("j =", j))
+      for (i in 1:I) {
+        if (i == 1) n.alloc[i] = 
+            j %/% prod(n.per.type[(i+1):I]) + min.type[i] else
+              if (i == I) n.alloc[i] = 
+                  j %% (n.per.type[I]) + min.type[i] else
+                    n.alloc[i] = 
+                      ((j %/% prod(n.per.type[(i+1):I])) %%
+                         n.per.type[i]) + min.type[i]
+      }
+      #print(n.alloc)
+      if (sum(n.alloc) == N[period]) alloc1(
+        M.now = M.now - n.alloc, 
+        period = period + 1, 
+        stem = cbind(stem, n.alloc), 
+        store.alloc = store.alloc) 
+    }
+  }
+}
+
+
+# Creat the allocation for your situation
+I = length(M)
+J = length(N)
+count = 0
+allocs = vector()
+alloc1(M, store.alloc=T)
+dim(allocs) = c(I, J, count)
+
+rand <-function(M,N,allocs,k){
+  ord <-c()
+  for (i in 1:length(M)){
+    deno <-c()
+    for (j in 1:length(N)){
+      temp <-c()
+      temp <- rep(j,allocs[i,j,k])
+      deno <- c(deno,temp)
+    }
+    ord <- c(ord,deno)
+  }
+  return(ord)
+}
+Tx <- matrix(ncol=sum(M), nrow=count)
+for(i in 1:count){
+  Tx[i,]<-rand(M,N,allocs,i)}
+
+multi <- function(M,N,allocs,k){
+  # k is the kth unique allocation you want to evaluate
+  temp <-c()
+  final <-c()
+  for (i in 1:length(M)){
+    for (j in 1:length(N)){
+      temp[j] <- factorial(allocs[i,j,k])
+      deno <- prod(temp)
+    }
+    final[i] <- factorial(M[i])/deno
+  }
+  return(prod(final))
+}
+
+
+# compute the multiplicities
+v <- c()
+for(i in 1:count){
+  v[i]<-multi(M,N,allocs,i)}
 w<-v/sum(v)
 
-## weights
+
